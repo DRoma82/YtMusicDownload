36f74317da16631dcf5d6ce038a1bd6c84176930
Danny Roma <daniel.roma@gmail.com>
2025-01-02 15:08:47 -0800

    (no message)

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 from yt_audio import yt_audio
+import argparse
 
 
 def main():
-    yt = yt_audio('https://www.youtube.com/watch?v=z3JhXn_onsw')
+    parser = argparse.ArgumentParser(description='Download audio from YouTube video')
+    parser.add_argument('url', help='YouTube video URL')
+    args = parser.parse_args()
+    yt = yt_audio(args.url)
 
     filename, buffer = yt.download_audio()
     with open(filename, 'wb') as f:
